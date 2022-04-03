3b10243ad06ee91f8ac98888ac83c4df36b8519d
Katie Fluck <info@fluckit.net>
2022-04-03 15:05:07 -0700

    Adds Dog file

--- src/main/java/com/quintrix/jfs/App.java
+++ src/main/java/com/quintrix/jfs/App.java
@@ -1,30 +1,37 @@
 package com.quintrix.jfs;
 
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Hello world!
  *
  */
 public class App {
-	
-    public static void main( String[] args ) {
-    	
-    	long biggerNum = 1000;
 
-    	boolean checkNum = false;
-    	
-    	int carsCounter = 0;
-    	
-    	char alphabet = 7;
-    	
-       
-        carsCounter = 2;
-        carsCounter++;
-        
-        System.out.println( "Katie is Great \n" + alphabet);
-        
-        
-        
-    }
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		// long biggerNum = 1000;
+
+		// boolean checkNum = false;
+
+		// int carsCounter = 0;
+
+		// char alphabet = 7;
+
+		Date curDate = new Date();
+
+		// System.out.println("It is " + curDate);
+		DateFormat dateFormate = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+		String strDate = dateFormate.format(curDate);
+		System.out.println("The date is " + strDate);
+
+		// carsCounter = 2;
+		// carsCounter++;
+
+		// System.out.println("Katie is Great \n" + alphabet);
+
+	}
 }
---
+++ src/main/java/com/quintrix/jfs/Dog.java
@@ -0,0 +1,26 @@
+package com.quintrix.jfs;
+
+public class Dog {
+
+	public static void main(String args[]) {
+
+		private String breed;
+		private int age;
+		private String color;
+		
+		
+		public String getBreed()  {
+			return breed;
+		}
+		
+		public void setBreed(String bredd)  {
+			this.breed = breed;
+		}
+		
+		public int getAge()  {
+			return age;
+		}
+		
+		public void 
+	}
+}
