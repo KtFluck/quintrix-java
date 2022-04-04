6e3aa93173099843011b480b9097cac52323266a
Katie Fluck <info@fluckit.net>
2022-04-03 18:36:49 -0700

    Update to dog file

--- src/main/java/com/quintrix/jfs/App.java
+++ src/main/java/com/quintrix/jfs/App.java
@@ -5,33 +5,55 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * Hello world!
+ * Hello world! and test objects
  *
  */
 public class App {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
+
 		// long biggerNum = 1000;
 
 		// boolean checkNum = false;
 
-		// int carsCounter = 0;
+		/*
+		 * String str1 = new String("Hello"); //creates new object therefore not equal
+		 * to str2 String str2 = new String("Hello");
+		 * 
+		 * if (str1 == str2) { System.out.println("They are equal"); } else {
+		 * System.out.println("They are not equal"); }
+		 */
 
-		// char alphabet = 7;
+		// Integer carsCounter1 = 10; // points to same value as carsCounter2 therefore
+		// equal
+		// Integer carsCounter2 = 10;
+		// if (carsCounter1.intValue() == carsCounter2.intValue()) {
+
+		// System.out.println("They are equal");
+		// } else {
+		// System.out.println("They are not equal");
+		// }
+
+		char alphabet = '\u2764'; // prints heart from unicode
+
+		// double testDecimalVal = 0.1;
+		// testDecimalVal = testDecimalVal + 0.2;
 
 		Date curDate = new Date();
 
 		// System.out.println("It is " + curDate);
-		DateFormat dateFormate = new SimpleDateFormat("yyyy-MM-dd hh:mm");
-		String strDate = dateFormate.format(curDate);
+
+		DateFormat dateFormate = new SimpleDateFormat("MM-dd-yyyy hh:mm"); // creates date format
+		String strDate = dateFormate.format(curDate); // grabs data from Date stored in curDate variable and applies
+														// formating
 		System.out.println("The date is " + strDate);
 
 		// carsCounter = 2;
 		// carsCounter++;
 
-		// System.out.println("Katie is Great \n" + alphabet);
+		System.out.println("Katie is Great \n" + alphabet);
 
 	}
 }
